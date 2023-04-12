f688f5654544feb9ca74ed48baf27fd7fa34dca8
Ruben <rtafoya@csumb.edu>
2023-04-11 19:27:04 -0700

    Rest and MQ changes

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,9 +1,13 @@
 package com.cst438.controllers;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,7 @@ import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class AssignmentController {
 
 	@Autowired
@@ -23,6 +28,34 @@ public class AssignmentController {
 	
 	@Autowired
 	CourseRepository courseRepository;
+	
+	@PostMapping("/assignment")
+	@Transactional
+	public AssignmentDTO newAssignment(@RequestBody AssignmentDTO dto) {
+		String userEmail = "dwisneski@csumb.edu";
+		// validate course and that the course instructor is the user
+		Course c = courseRepository.findById(dto.courseId).orElse(null);
+		if (c != null && c.getInstructor().equals(userEmail)) {
+			// create and save new assignment
+			// update and return dto with new assignment primary key
+			Assignment a = new Assignment();
+			a.setCourse(c);
+			a.setName(dto.name);
+//			a.setDueDate(Date.valueOf(dto.dueDate));
+			a.setDueDate(dto.dueDate);
+			a.setNeedsGrading(1);
+			a = assignmentRepository.save(a);
+			dto.assignmentId=a.getId();
+			return dto;
+			
+		} else {
+			// invalid course
+			throw new ResponseStatusException( 
+                           HttpStatus.BAD_REQUEST, 
+                          "Invalid course id.");
+		}
+	}
+
 	
 	@PutMapping("/addAssignment")
 	@Transactional
